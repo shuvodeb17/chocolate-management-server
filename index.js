9a1e9cf4a903801303a703754c499a6435f0e965
Shuvo Deb <shuvodeb17014@gmail.com>
2023-05-09 15:47:15 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
+require('dotenv').config()
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
@@ -6,6 +8,37 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.b0yctrm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
